35c48acdd9d0cd89379e454f952ed9bc7f9835ae
Lammy8 <harrison.lam@connect.qut.edu.au>
2026-01-05 12:57:04 +1000

    Initial setup: Vite+React+TS scaffold and cleanup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>My Portfolio</h1>
+      </header>
+      <main>
+        <p>Work in progress. Loading amazing content...</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
